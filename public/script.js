cda884fa07609e6376269937715aee08ab64dfd2
Mac Julian Samuel <macjuliansamuel@Macs-MacBook-Air.local>
2021-07-17 22:36:28 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets3.lottiefiles.com/packages/lf20_HX0isy.json' // lottie file path
+  })
